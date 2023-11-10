625a8d217783f3d1b306b279c7399579ba3f5f54
Joshua Yeh <joshua.yeh@misd400.org>
2023-11-09 17:20:02 -0800

    We did more committing
    This is for intake btw

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import java.util.Map;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+// import frc.robot.Constants.IntakeConstants;
+
+public class IntakeSubsystem {
+    TalonFX f;
+}
